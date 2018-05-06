b25805e4ad9d54febd5daacffd56095c9a7713fe
hridayns <hridaysanghvi@gmail.com>
2018-05-06 19:13:49 +0530

    code clean up

--- blur_detector.py
+++ blur_detector.py
@@ -6,7 +6,6 @@ import pandas as pd
 from sklearn.neural_network import MLPClassifier
 from sklearn.metrics import accuracy_score
 
-debugLim = 5
 dir_path = os.path.dirname(os.path.realpath(__file__))
 
 #--------------------------------------------TRAIN SCRIPT-------------------------------------------------------------------------
@@ -40,11 +39,9 @@ def load_train_images(folders):
 				xdata.append(v)
 				ydata.append(y_val)
 				i = i + 1
-			# if i == debugLim:
-			# 	break
 	xdata_arr = np.asarray(xdata)
 	ydata_arr = np.asarray(ydata)
-	return np.reshape(xdata_arr,(xdata_arr.shape[0],1)),ydata_arr#np.reshape(ydata_arr,(ydata_arr.shape[0],1))
+	return np.reshape(xdata_arr,(xdata_arr.shape[0],1)),ydata_arr
 
 
 train_data_folders = [artificial_blur_path,natural_blur_path,undistorted_path]
@@ -62,17 +59,11 @@ natural_blur_path = os.path.join(eval_folder,natural_blur_folder)
 natural_blur_file = os.path.join(eval_folder,'NaturalBlurSet.xlsx')
 digital_blur_file = os.path.join(eval_folder,'DigitalBlurSet.xlsx')
 
-# print(digital_blur_path)
-# print(natural_blur_path)
-
 natural_blur_data = pd.read_excel(natural_blur_file)
 digital_blur_data = pd.read_excel(digital_blur_file)
 
-# print(natural_blur_data.head())
 digital_blur_data.rename(columns={'Unnamed: 1':'Blur Label'}, inplace=True)
 digital_blur_data.rename(columns={'MyDigital Blur':'Image Name'}, inplace=True)
-# print(digital_blur_data.head())
-
 
 def load_test_images(folders):
 	xdata = []
@@ -83,7 +74,6 @@ def load_test_images(folders):
 			df = natural_blur_data
 		else:
 			df = digital_blur_data
-		# print(df.loc[0:5,'Image Name'])
 		for filename in os.listdir(folder):
 			if folder == natural_blur_path:
 				img_name = path.splitext(filename)[0]
@@ -99,25 +89,10 @@ def load_test_images(folders):
 					if(img_name in val['Image Name']):
 						img_idx = idx
 						break
-				# print(img_idx)
-					# print(img_name in row[0])
-				# cond = (img_name in df.loc[0,'Image Name'])
-				# print(img_name)
-				# print(cond)
-				# print(img_name,' == ',filename)
-				# cond = (img_name in df.loc[:,'Image Name'])
-				# print(len(df.iloc[0,0]),' == ',len(img_name))
-				# print(df.iloc[0,0],' == ',img_name)
-				# print(img_name in df.iloc[0,0])
-				# idx = df.index[cond]
-				# print(idx)
 
 				y_val = df.iloc[img_idx,1]
-				# print(y_val)
 				ydata.append(y_val)
 				i = i + 1
-				# if i == debugLim:
-				# 	break
 	xdata_arr = np.asarray(xdata)
 	ydata_arr = np.asarray(ydata)
 	return np.reshape(xdata_arr,(xdata_arr.shape[0],1)),np.reshape(ydata_arr,(ydata_arr.shape[0],1))
@@ -130,7 +105,4 @@ mlp.fit(X_train,y_train)
 
 predictions = mlp.predict(X_test)
 
-print('acc by score(): ', mlp.score(X_test,y_test))
-print('acc by accuracy_score(): ', accuracy_score(y_test, predictions))
-
-
+print('Accuracy: ', mlp.score(X_test,y_test) * 100.0)
